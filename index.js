7cf7f8789c2226ce643d26d6f6c1f396f0af6acc
Jonathan Ramses <abjonathan@hotmail.com>
2025-10-25 00:12:55 -0300

    firts commit

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ app.get('/', (req, res) => {
   res.send('API de Conversão de Áudio está funcionando!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
